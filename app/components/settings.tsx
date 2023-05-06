7b695d3013009941e2bf0a4f50af729397458d73
mfxiaosheng <1211222334@qq.com>
2023-05-06 15:06:10 +0800

    Revert "去掉git信息"
    This reverts commit bbb6ffbac7d88c3caa1cb29eae2ddf3411f10eb7.

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -212,7 +212,18 @@ export function Settings() {
 
   function checkUpdate(force = false) {
     setCheckingUpdate(true);
-    
+    updateStore.getLatestVersion(force).then(() => {
+      setCheckingUpdate(false);
+    });
+
+    console.log(
+      "[Update] local version ",
+      new Date(+updateStore.version).toLocaleString(),
+    );
+    console.log(
+      "[Update] remote version ",
+      new Date(+updateStore.remoteVersion).toLocaleString(),
+    );
   }
 
   const usage = {
--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -146,6 +146,11 @@ export function SideBar(props: { className?: string }) {
             />
           </div>
           <div className={styles["sidebar-action"]}>
+            <Link to={Path.Settings}>
+              <IconButton icon={<SettingsIcon />} shadow />
+            </Link>
+          </div>
+          <div className={styles["sidebar-action"]}>
             <a href={REPO_URL} target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
